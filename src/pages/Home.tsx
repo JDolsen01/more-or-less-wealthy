ab80571c37bc96150a0fe407d2baac994a4fc0e5
Jordan Olsen <2001jordanolsen@gmail.com>
2026-01-07 14:51:57 -0700

    add navbar to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,22 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
-      <div>Home Page</div>
-      <Link to="/signup">Signup</Link>
-      <br></br>
-      <Link to="/login">Login</Link>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <Link className="btn btn-ghost text-xl" to="/">
+            More || Less Wealthy
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <Link className="btn" to="/login">
+            Login
+          </Link>
+          <Link className="btn btn-primary ml-2" to="/signup">
+            Signup
+          </Link>
+        </div>
+      </div>
+      <h1>Home Page</h1>
     </>
   );
 }
