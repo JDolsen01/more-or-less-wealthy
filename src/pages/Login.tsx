62b8615d3a333f0455174691fd07a6360060aff3
Jordan Olsen <2001jordanolsen@gmail.com>
2026-01-06 17:59:24 -0700

    Basic styling for login/register and site title changed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -32,32 +32,66 @@ function Login() {
 
   return (
     <div>
-      <h2>Login Page</h2>
-      <br></br>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        ></input>
-        <br></br>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        ></input>
-        <br></br>
-        <button type="submit">Login</button>
-        <br></br>
-        {message && <p>{message}</p>}
-      </form>
-      <div>Don't have an account?</div>
-      <br></br>
-      <Link to="/register">Register</Link>
+      <div className="navbar bg-base-100 shadow-sm fixed">
+        <Link className="btn btn-ghost text-xl" to="/">
+          More || Less Wealthy
+        </Link>
+      </div>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h2 className="pb-4">Login Page</h2>
+        <form onSubmit={handleLogin}>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <legend className="fieldset-legend">Login</legend>
+
+            <label className="label">Email</label>
+            <input
+              type="email"
+              className="input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required
+            />
+
+            <label className="label">Password</label>
+            <input
+              type="password"
+              className="input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              required
+            />
+            <button type="submit" className="btn btn-neutral mt-4">
+              Login
+            </button>
+            {message && (
+              <div role="alert" className="alert alert-error">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>{message}</span>
+              </div>
+            )}
+            <div className="pt-4">
+              Don't have an account?{" "}
+              <Link to="/register" className="link">
+                Register
+              </Link>
+            </div>
+          </fieldset>
+        </form>
+      </div>
     </div>
   );
 }
