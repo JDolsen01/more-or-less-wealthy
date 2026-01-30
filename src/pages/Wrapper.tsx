63169cc68845031eccac4768805e5fa355aae366
Jordan Olsen <2001jordanolsen@gmail.com>
2026-01-29 17:42:06 -0700

    created modals for adding new items

--- src/pages/Wrapper.tsx
+++ src/pages/Wrapper.tsx
@@ -8,11 +8,23 @@ function Wrapper({ children }: { children: JSX.Element }) {
   const [authenticated, setAuthenticated] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
 
-  const modalRef = useRef<HTMLDialogElement>(null);
+  const incomeModal = useRef<HTMLDialogElement>(null);
+  const expenseModal = useRef<HTMLDialogElement>(null);
+  const recurringModal = useRef<HTMLDialogElement>(null);
+  const budgetModal = useRef<HTMLDialogElement>(null);
 
-  const handleOpenModal = () => {
-    if (modalRef.current) {
-      modalRef.current.showModal();
+  const handleOpenModal = (
+    modalType: "income" | "expense" | "recurring" | "budget"
+  ) => {
+    const modals = {
+      income: incomeModal,
+      expense: expenseModal,
+      recurring: recurringModal,
+      budget: budgetModal,
+    };
+
+    if (modals[modalType].current) {
+      modals[modalType].current.showModal();
     } else {
       console.error("Modal element not found");
     }
@@ -65,41 +77,85 @@ function Wrapper({ children }: { children: JSX.Element }) {
               {
                 label: "Income",
                 icon: "inboxPlus",
-                onClick: handleOpenModal,
+                onClick: () => handleOpenModal("income"),
               },
               {
                 label: "Expense",
                 icon: "inboxMinus",
-                onClick: () => {},
+                onClick: () => handleOpenModal("expense"),
               },
               {
                 label: "Recurring",
                 icon: "repeat",
-                onClick: () => {},
+                onClick: () => handleOpenModal("recurring"),
               },
               {
                 label: "Budget",
                 icon: "dollar",
-                onClick: () => {},
+                onClick: () => handleOpenModal("budget"),
               },
             ]}
+          />
+          <dialog
+            id="my_modal_5"
+            className="modal modal-bottom sm:modal-middle"
+            ref={incomeModal}
           >
-            <dialog
-              id="my_modal_5"
-              className="modal modal-bottom sm:modal-middle"
-              ref={modalRef}
-            >
-              <div className="modal-box">
-                <h3 className="font-bold text-lg">Add Income</h3>
-                <p className="py-4">
-                  Press ESC key or click the button below to close
-                </p>
-              </div>
-              <form method="dialog" className="modal-backdrop">
-                <button>close</button>
-              </form>
-            </dialog>
-          </Fab>
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Add Income</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+              <button>close</button>
+            </form>
+          </dialog>
+          <dialog
+            id="my_modal_5"
+            className="modal modal-bottom sm:modal-middle"
+            ref={expenseModal}
+          >
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Add Expense</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+              <button>close</button>
+            </form>
+          </dialog>
+          <dialog
+            id="my_modal_5"
+            className="modal modal-bottom sm:modal-middle"
+            ref={recurringModal}
+          >
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Add Recurring Transaction</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+              <button>close</button>
+            </form>
+          </dialog>
+          <dialog
+            id="my_modal_5"
+            className="modal modal-bottom sm:modal-middle"
+            ref={budgetModal}
+          >
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Add Budget</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+              <button>close</button>
+            </form>
+          </dialog>
           {children}
         </div>
       );
